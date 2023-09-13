5a94d6dae588a476ff76cfd76c014dca1000391a
RKanna <rkannan.balakrishnan@hotmail.com>
2023-09-13 06:44:37 +0530

    addEventListener added

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //1.You are given a list of integers. Write a program to find the maximum product of three integers in the list and display the result on a web page.
-function findMaxProduct() {
+const btnSubmit = document.getElementById("btnSubmit");
+btnSubmit.addEventListener("click", function () {
   const numOne = document.getElementById("num1").value,
     numTwo = document.getElementById("num2").value,
     numThree = document.getElementById("num3").value,
@@ -16,65 +17,55 @@ function findMaxProduct() {
   );
   document.getElementById("result").textContent =
     "Maximum Product: " + maxProduct;
-}
+});
 
 //2.You are given a list of integers representing stock prices. Write a program to find the maximum profit that can be made by buying and selling the stock at most once, and display the result on a web page.
-// function findMaximumProfit() {
-//   const numOneProfit = document.getElementById("num7").value,
-//     numTwoProfit = document.getElementById("num8").value,
-//     numThreeProfit = document.getElementById("num9").value,
-//     numFourProfit = document.getElementById("num10").value,
-//     numFiveProfit = document.getElementById("num11").value,
-//     numSixProfit = document.getElementById("num12").value,
-//     resultProfit = document.getElementById("result"),
-//     btnProfit = document.getElementById("btnSubmit");
-// }
-// Function to find the maximum profit from stock prices
-function findMaximumProfit() {
-  const stockPrices = [
-    Number(document.getElementById("num7").value),
-    Number(document.getElementById("num8").value),
-    Number(document.getElementById("num9").value),
-    Number(document.getElementById("num10").value),
-    Number(document.getElementById("num11").value),
-    Number(document.getElementById("num12").value),
-  ];
+// const btnSubmitProfit = document.getElementById("btnSubmitProfit");
+// btnSubmitProfit.addEventListener("click", function () {
+//   const stockPrices = [
+//     Number(document.getElementById("num7").value),
+//     Number(document.getElementById("num8").value),
+//     Number(document.getElementById("num9").value),
+//     Number(document.getElementById("num10").value),
+//     Number(document.getElementById("num11").value),
+//     Number(document.getElementById("num12").value),
+//   ];
 
-  if (stockPrices.length < 2) {
-    resultProfit.textContent = "At least two stock prices are required.";
-    return;
-  }
+//   if (stockPrices.length < 2) {
+//     resultProfit.textContent = "At least two stock prices are required.";
+//     return;
+//   }
 
-  const maxProfit = calculateMaxProfit(stockPrices);
-  resultProfit.textContent = `Maximum profit: $${maxProfit}`;
-}
+//   const maxProfit = calculateMaxProfit(stockPrices);
+//   resultProfit.textContent = `Maximum profit: $${maxProfit}`;
 
-function calculateMaxProfit(prices) {
-  let maxProfit = 0;
-  let minPrice = prices[0];
+//   function calculateMaxProfit(prices) {
+//     let maxProfit = 0;
+//     let minPrice = prices[0];
 
-  for (let i = 1; i < prices.length; i++) {
-    const currentPrice = prices[i];
-    const potentialProfit = currentPrice - minPrice;
+//     for (let i = 1; i < prices.length; i++) {
+//       const currentPrice = prices[i];
+//       const potentialProfit = currentPrice - minPrice;
 
-    if (potentialProfit > maxProfit) {
-      maxProfit = potentialProfit;
-    }
+//       if (potentialProfit > maxProfit) {
+//         maxProfit = potentialProfit;
+//       }
 
-    if (currentPrice < minPrice) {
-      minPrice = currentPrice;
-    }
-  }
+//       if (currentPrice < minPrice) {
+//         minPrice = currentPrice;
+//       }
+//     }
 
-  return maxProfit;
-}
+//     return maxProfit;
+//   }
 
-const btnProfit = document.getElementById("btnSubmit");
-btnProfit.addEventListener("click", findMaximumProfit);
+//   const btnProfit = document.getElementById("btnSubmit");
+//   btnProfit.addEventListener("click", findMaximumProfit);
+// });
 
 //3.You are given a list of integers representing weights of items and their corresponding values. Write a program to find the maximum value that can be obtained by selecting a subset of items with a total weight not exceeding a given limit, and display the result on a web page.
-
-function maxFromSubsetOfItem() {
+const findoutput = document.getElementById("findoutput");
+findoutput.addEventListener("click", function () {
   const weightsInput = document.getElementById("weights").value;
   const valuesInput = document.getElementById("values").value;
   const weightLimit = parseInt(document.getElementById("weight-limit").value);
@@ -103,34 +94,35 @@ function maxFromSubsetOfItem() {
   const maxValue = dp[n][weightLimit];
   document.getElementById("resultSubset").textContent =
     "Maximum value: " + maxValue;
-}
+});
 
 //4.You are given a list of numbers. Write a program to find the subarray with the largest sum and display the result on a web page.
+const btnCalculateMaxSubArraySum = document.getElementById(
+  "btnCalculateMaxSubArraySum"
+);
+btnCalculateMaxSubArraySum.addEventListener("click", function () {
+  function findMaxSubarraySum(arr) {
+    let maxSum = arr[0];
+    let currentSum = arr[0];
+    let start = 0;
+    let subarray = [arr[0]];
 
-function findMaxSubarraySum(arr) {
-  let maxSum = arr[0];
-  let currentSum = arr[0];
-  let start = 0;
-  let subarray = [arr[0]];
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] > currentSum + arr[i]) {
+        currentSum = arr[i];
+        start = i;
+      } else {
+        currentSum += arr[i];
+      }
 
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > currentSum + arr[i]) {
-      currentSum = arr[i];
-      start = i;
-    } else {
-      currentSum += arr[i];
+      if (currentSum > maxSum) {
+        maxSum = currentSum;
+        subarray = arr.slice(start, i + 1);
+      }
     }
 
-    if (currentSum > maxSum) {
-      maxSum = currentSum;
-      subarray = arr.slice(start, i + 1);
-    }
+    return { maxSum, subarray };
   }
-
-  return { maxSum, subarray };
-}
-
-function calculateMaxSubarraySum() {
   const inputElement = document.getElementById("inputArray");
   const inputArray = inputElement.value
     .split(",")
@@ -140,57 +132,60 @@ function calculateMaxSubarraySum() {
 
   const resultElement = document.getElementById("resulthere");
   resultElement.innerHTML = `The subarray with the largest sum is [${subarray}] with a sum of ${maxSum}.`;
-}
+});
 
 //5.You are given a binary tree. Write a program to find the maximum depth of the tree and display the result on a web page.
-function TreeNode(val) {
-  this.val = val;
-  this.left = this.right = null;
-}
+const calculateDepth = document.getElementById("calculateDepth");
+calculateDepth.addEventListener("click", function () {
+  function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+  }
 
-function createBinaryTree(arr) {
-  if (arr.length === 0) return null;
+  function createBinaryTree(arr) {
+    if (arr.length === 0) return null;
 
-  const val = arr.shift();
-  if (val === null) return null;
+    const val = arr.shift();
+    if (val === null) return null;
 
-  const root = new TreeNode(val);
-  root.left = createBinaryTree(arr);
-  root.right = createBinaryTree(arr);
+    const root = new TreeNode(val);
+    root.left = createBinaryTree(arr);
+    root.right = createBinaryTree(arr);
 
-  return root;
-}
+    return root;
+  }
 
-function maxDepth(root) {
-  if (!root) return 0;
+  function maxDepth(root) {
+    if (!root) return 0;
 
-  const leftDepth = maxDepth(root.left);
-  const rightDepth = maxDepth(root.right);
+    const leftDepth = maxDepth(root.left);
+    const rightDepth = maxDepth(root.right);
 
-  return Math.max(leftDepth, rightDepth) + 1;
-}
+    return Math.max(leftDepth, rightDepth) + 1;
+  }
 
-function findMaxDepth() {
-  const inputField = document.getElementById("inputField");
-  const input = inputField.value.trim();
-  const inputArray = input.split(",").map((s) => {
-    if (s.trim() === "null") return null;
-    return parseInt(s);
-  });
+  function findMaxDepth() {
+    const inputField = document.getElementById("inputField");
+    const input = inputField.value.trim();
+    const inputArray = input.split(",").map((s) => {
+      if (s.trim() === "null") return null;
+      return parseInt(s);
+    });
 
-  const treeRoot = createBinaryTree(inputArray);
-  const depth = maxDepth(treeRoot);
+    const treeRoot = createBinaryTree(inputArray);
+    const depth = maxDepth(treeRoot);
 
-  const maxDepthSpan = document.getElementById("maxDepth");
-  maxDepthSpan.textContent = depth.toString();
-}
+    const maxDepthSpan = document.getElementById("maxDepth");
+    maxDepthSpan.textContent = depth.toString();
+  }
 
-const calculateButton = document.querySelector("button");
-calculateButton.addEventListener("click", findMaxDepth);
+  const calculateButton = document.querySelector("button");
+  calculateButton.addEventListener("click", findMaxDepth);
+});
 
 //6.You are given a list of integers. Write a program to find the kth smallest element in the list and display the result on a web page
-
-function findKthSmallest() {
+const findKthSmallest = document.getElementById("findKthSmallest");
+findKthSmallest.addEventListener("click", function () {
   const inputList = document.getElementById("inputList").value;
   const numbers = inputList.split(",").map(Number);
   numbers.sort((a, b) => a - b);
@@ -199,10 +194,11 @@ function findKthSmallest() {
   document.getElementById(
     "resultKth"
   ).innerText = `The ${kValue}th smallest element in the list is ${kthSmallest}`;
-}
+});
 
 //7.You are given a string.Write a program that checks if a given string is a valid email address using logical operators and displays the result in web page.
-function validateEmail() {
+const validateEmailBtn = document.getElementById("validateEmailBtn");
+validateEmailBtn.addEventListener("click", function () {
   const email = document.getElementById("emailInput").value;
   const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
   if (emailRegex.test(email)) {
@@ -210,7 +206,7 @@ function validateEmail() {
   } else {
     document.getElementById("resultEmail").textContent = "Invalid email";
   }
-}
+});
 
 //8.Write a program to calculate the total sum of a particular column in the table and display it on the web page.
 
@@ -238,8 +234,8 @@ let result = removePunctuation(inputSentence);
 document.getElementById("resultPunctuation").textContent = result;
 
 //10.You are given a list of words. Write a program that finds the words that have more than 5 letters and displays the result on a web page.
-
-function findWords() {
+const findWordsBtn = document.getElementById("findWordsBtn");
+findWordsBtn.addEventListener("click", function () {
   let inputText = document.getElementById("inputWords").value;
   let words = inputText.split(" ");
   let longWords = words.filter(function (word) {
@@ -248,4 +244,4 @@ function findWords() {
   let resultDiv = document.getElementById("resultWords");
   resultDiv.innerHTML =
     "<p>Words with more than 5 letters:</p><p>" + longWords.join(", ") + "</p>";
-}
+});
